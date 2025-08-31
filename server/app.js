261a0cffc09099e4d7b563df42f0588d0b4db327
Mo-create-git <msaffan078@gmail.com>
2025-08-31 14:48:56 +0530

    end of http

--- server/app.js
+++ server/app.js
@@ -15,48 +15,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send('HELLO , EXPRESS')
 })
-// Dynamic Routes
-
-app.use('/user', router)
-
-// Handling a GET request;
-/* used to fetch data from the server
-
-Handling a post req;
-used to send data to the server and create a new resource.
-requires middleware {express.json()}to handle json inpur
-*/
-
-// here express.json() is middleware;
 
 
-
-app.post('/users', express.json(), (req, res) => {
-    const { name, email } = req.body;
+app.get('/things/:name/:id', (req,res) => {
+    const {name, id} = req.params
     res.json({
-        message: `User ${name} with email ${email} created successfully`
-    });
-})
-
-// Handling a put req;
-app.put('/users/:id', (req, res) => {
-    const userId = req.params.id
-    const { name, email } = req.body
-    res.json({
-        message: `User ${userId} updated to ${name}, ${email}`
-    })
-}) // here i've been stuck for an hour figuring out what the error is the error was "app.use(express.json())"
-
-// Handlinga delete req;
-/* use to send data to the server and create a new resource.
-Requires middleware (express.json()) to handle JSON input.*/
-
-app.delete('/users/:id', (req, res) => {
-    const userId = req.params.id
-    res.json({
-        message: `User with ID ${userId} deleted successfully`
+        id,
+        name
     })
 })
+
 
 
 
